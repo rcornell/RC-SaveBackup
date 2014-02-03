42d98f42a5d2c482b64d55a3d859db9639f38fef
rcornell <rob.cornell@gmail.com>
2014-02-03 18:19:25 -0500

    Added WrapPanelGame class

---
+++ Saved Game Backup/WrapPanelGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Saved_Game_Backup
+{
+    public class WrapPanelGame : Control {
+
+
+
+
+
+        public Game Game {
+            get { return (Game)GetValue(GameProperty); }
+            set { SetValue(GameProperty, value); }
+        }
+
+        public static readonly DependencyProperty GameProperty =
+            DependencyProperty.Register("Game", typeof(Game), typeof(WrapPanelGame), new PropertyMetadata(default(Game)));
+
+
+        public ImageSource Thumbnail {
+            get { return (ImageSource)GetValue(ThumbnailProperty); }
+            set { SetValue(ThumbnailProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThumbnailProperty =
+            DependencyProperty.Register("Thumbnail", typeof(ImageSource), typeof(WrapPanelGame), new PropertyMetadata(0));
+
+
+
+        public double ThumbnailHeight {
+            get { return (double)GetValue(ThumbnailHeightProperty); }
+            set { SetValue(ThumbnailHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThumbnailHeightProperty =
+            DependencyProperty.Register("ThumbnailHeight", typeof(double), typeof(WrapPanelGame), new PropertyMetadata(0));
+
+
+
+        public double ThumbnailWidth {
+            get { return (double)GetValue(ThumbnailWidthProperty); }
+            set { SetValue(ThumbnailWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThumbnailWidthProperty =
+            DependencyProperty.Register("ThumbnailWidth", typeof(double), typeof(WrapPanelGame), new PropertyMetadata(0));
+
+
+
+        public Stretch Stretch {
+            get { return (Stretch)GetValue(StretchProperty); }
+            set { SetValue(StretchProperty, value); }
+        }
+
+        public static readonly DependencyProperty StretchProperty =
+            DependencyProperty.Register("Stretch", typeof(Stretch), typeof(WrapPanelGame), new PropertyMetadata(default(Stretch)));
+
+
+
+        public StretchDirection StretchDirection {
+            get { return (StretchDirection)GetValue(StretchDirectionProperty); }
+            set { SetValue(StretchDirectionProperty, value); }
+        }
+
+        public static readonly DependencyProperty StretchDirectionProperty =
+            DependencyProperty.Register("StretchDirection", typeof(StretchDirection), typeof(WrapPanelGame), new PropertyMetadata(default(StretchDirection)));
+
+
+
+        public new bool IsMouseOver {
+            get { return (bool)GetValue(IsMouseOverProperty); }
+            set { SetValue(IsMouseOverProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsMouseOverProperty =
+            DependencyProperty.Register("IsMouseOver", typeof(bool), typeof(WrapPanelGame), new PropertyMetadata(default(bool)));
+
+
+
+        public bool IsSelected {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(WrapPanelGame), new PropertyMetadata(default(bool)));
+    }
+}
