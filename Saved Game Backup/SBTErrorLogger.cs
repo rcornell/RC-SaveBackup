ee1e9093745933207c6f46e51b5120ac4c291228
rcornell <rob.cornell@gmail.com>
2014-02-22 01:40:36 -0500

    Changed SBTErrorLogger to make a new log with a date stamp

--- Saved Game Backup/SBTErrorLogger.cs
+++ Saved Game Backup/SBTErrorLogger.cs
@@ -8,21 +8,30 @@ using System.Threading.Tasks;
 namespace Saved_Game_Backup
 {
 // ReSharper disable once InconsistentNaming
-    public class SBTErrorLogger
-    {
+    public class SBTErrorLogger {
+        private static readonly string MyDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private const string SbtPath = "\\Save Backup Tool\\Error\\";
+
         public static void Log(Exception ex) {
 
             var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            Directory.CreateDirectory(documentsPath + "\\Save Backup Tool\\Error\\");
+            Directory.CreateDirectory(MyDocs + SbtPath);
             var sb = new StringBuilder();
             sb.AppendLine(ex.Message);
             //if (!string.IsNullOrWhiteSpace(ex.InnerException.Message)) sb.AppendLine(ex.InnerException.Message);
             //if (!string.IsNullOrWhiteSpace(ex.Source)) sb.AppendLine(ex.Source);
             // if (!string.IsNullOrWhiteSpace(ex.StackTrace)) sb.AppendLine(ex.StackTrace);
-            using (var fs = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Save Backup Tool\\Error\\Log.txt"))
-            {
-                using (var sw = new StreamWriter(fs))
-                {
+            var day = DateTime.Now.Day;
+            var month = DateTime.Now.Month;
+            var year = DateTime.Now.Year;
+            var hour = DateTime.Now.Hour;
+            var minute = DateTime.Now.Minute;
+            var second = DateTime.Now.Second;
+            var millisecond = DateTime.Now.Millisecond;
+            var dateString = string.Format(@" {0}-{1}-{2} {3}-{4}-{5}-{6}", month, day, year, hour, minute, second, millisecond);
+            var writePath = string.Format(@"{0}{1}Log {2}.txt",MyDocs, SbtPath, dateString);
+            using (var fs = File.OpenWrite(writePath)) {
+                using (var sw = new StreamWriter(fs)) {
                     sw.WriteLineAsync(sb.ToString());
                 }
 
