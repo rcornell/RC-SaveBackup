3c4058b539b4b378e55518aa683fd73ddd00c7ff
rcornell <rob.cornell@gmail.com>
2014-02-23 11:11:48 -0500

    Made pathcompare class

---
+++ Saved Game Backup/PathCompare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saved_Game_Backup
+{
+    public class PathCompare
+    {
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+
+        public PathCompare(string source, string destination) {
+            SourcePath = source;
+            DestinationPath = destination;
+        }
+    }
+}
