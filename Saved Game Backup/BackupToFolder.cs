02eadca2d4411985c5b3051e19553ad017ac3a2f
rcornell <rob.cornell@gmail.com>
2014-02-28 17:17:36 -0500

    Added new BackupXXX classes to spread

---
+++ Saved Game Backup/BackupToFolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saved_Game_Backup
+{
+    public class BackupToFolder
+    {
+    }
+}
