b06765988f95b5636eb22c0fad7f9ae041815ce4
rcornell <rob.cornell@gmail.com>
2014-03-07 19:23:44 -0500

    Reorganized folders and added Dop

---
+++ Saved Game Backup/OnlineStorage/DropBoxAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using DropNet;
+
+namespace Saved_Game_Backup.OnlineStorage
+{
+    class DropBoxAPI {
+
+        public static async Task DropLogin() {
+            
+        }
+
+        public static void GetPermission(string url) {
+            
+        }
+
+        public async Task Upload(string folderPath, FileInfo file) {
+            
+        }
+
+        public async Task Download(string filePath, string targetFile) {
+            
+        }
+
+
+    }
+}
